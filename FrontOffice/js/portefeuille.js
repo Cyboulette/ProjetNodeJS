d86be35a3c889190f0eeff478cc8269ca3602266
Unknown <holx46@gmail.com>
2018-04-04 17:35:43 +0200

    Front

--- FrontOffice/js/portefeuille.js
+++ FrontOffice/js/portefeuille.js
@@ -5,13 +5,15 @@ var portefeuille = new Vue({
   data : {
     monnaies: null,
     selected: null,
-    nbCrypto: null
+    nbCrypto: null,
+    tableau: []
   },
   created: function() {
     var vm = this;
     this.$http.get('http://localhost:8080/api/monnaies').then(function(res) {
       vm.monnaies = res.data.data;
     });
+    this.updatePortefeuille();
   },
   methods: {
     updateGraph:function(){
@@ -43,31 +45,63 @@ var portefeuille = new Vue({
                 }
               }]
             });
-            // vm.titre = "Bourse"
-            // vm.tableau = "<td>"+ vm.selected +"</td><td>"+points[points.length-1][1]+"€</td>";
           }else{
             $('#graphique').html('<div class="alert alert-info">Portefeuille Vide</div>');
-            this.tableau = null;
-            this.titre = null;
           }
         });
       }else{
         $('#graphique').html("");
-        this.tableau = null;
-        this.titre = null;
       }
     },
     buy:function(){
       var vm = this;
       if(vm.selected != "null") {
-        this.$http.post('http://localhost:8080/api/portefeuille/'+ vm.selected, {amount: this.$refs.input_buy.value}).then(function(res) {
-          if(res.data.success == true){
-            $('#alert-buy').html('<div class="alert alert-success">Achat effectué !</div><div class="badge badge-primary">Solde en Euros :'+ res.data.soldeRestantEuros+'€</div><div class="badge badge-primary">Solde '+ vm.selected+':'+ res.data.soldeRestantEuros+'</div>');
-          }else{
-            $('#alert-buy').html('<div class="alert alert-danger"> '+ res.data.message +' </div>');
-          }
-        });
+        if(this.$refs.input_buy.value.length != 0){
+          this.$http.post('http://localhost:8080/api/portefeuille/'+ vm.selected, {amount: this.$refs.input_buy.value}).then(function(res) {
+            if(res.data.success == true){
+              $('#alert-buy').html('<div class="alert alert-success">Achat effectué !</div>');
+            }else{
+              $('#alert-buy').html('<div class="alert alert-danger"> '+ res.data.message +' </div>');
+            }
+            vm.updatePortefeuille();
+          });
+        }else {
+          $('#alert-buy').html('<div class="alert alert-danger">Saisissez un montant </div>');
+        }
+      }else{
+        $('#alert-buy').html('<div class="alert alert-danger">Séléctionnez une monnaie </div>');
       }
+    },
+    sell:function(){
+      var vm = this;
+      if(vm.selected != "null") {
+        if(this.$refs.input_sell.value.length != 0){
+          this.$http.post('http://localhost:8080/api/portefeuille/'+ vm.selected+'/vendre', {amount: this.$refs.input_sell.value}).then(function(res) {
+            if(res.data.success == true){
+              $('#alert-sell').html('<div class="alert alert-success">Vente effectuée !</div>');
+            }else{
+              $('#alert-sell').html('<div class="alert alert-danger"> '+ res.data.message +' </div>');
+            }
+            vm.updatePortefeuille();
+          });
+        }else {
+          $('#alert-sell').html('<div class="alert alert-danger">Saisissez un montant </div>');
+        }
+      }else{
+        $('#alert-sell').html('<div class="alert alert-danger">Séléctionnez une monnaie </div>');
+      }
+    },
+    updatePortefeuille:function(){
+      this.tableau.length = 0;
+      var vm = this;
+      this.$http.get('http://localhost:8080/api/portefeuille/').then(function(res) {
+        console.log(res.data.data);
+        for(var mon in res.data.data){
+          var data = res.data.data[mon];
+          vm.tableau.push("<td>"+ data.name +"</td><td>"+data.solde+"</td>");
+        }
+      });
+
     }
   }
 });
